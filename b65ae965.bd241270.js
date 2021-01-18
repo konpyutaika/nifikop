11a1fe16f017b86e82c312417136e2c7731a9f1e
erdrix <alexandre.guitton@orange.com>
2021-01-18 14:21:53 +0000

    [skip ci]

--- b65ae965.bd241270.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[66],{129:function(e){e.exports=JSON.parse('{"allTagsPath":"/nifikop/blog/tags","slug":"kubernetes","name":"kubernetes","count":2,"permalink":"/nifikop/blog/tags/kubernetes"}')}}]);
---
+++ eabf6dbb.03f50cee.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[89],{193:function(p){p.exports=JSON.parse('["v0.4.3"]')}}]);
