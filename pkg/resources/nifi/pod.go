dfcb88cc5a423c43fa8af7c4a96b329336544060
Juldrixx <31806759+juldrixx@users.noreply.github.com>
2022-03-15 22:20:21 +0100

    [Feature/NiFiCluster] Add support topologySpreadConstraint (#18)
    * Adds support topologySpreadConstraint
    
    * Update changelog

--- pkg/resources/nifi/pod.go
+++ pkg/resources/nifi/pod.go
@@ -2,10 +2,11 @@ package nifi
 
 import (
 	"fmt"
-	configcommon "github.com/konpyutaika/nifikop/pkg/nificlient/config/common"
-	runtimeClient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sort"
 	"strings"
+
+	configcommon "github.com/konpyutaika/nifikop/pkg/nificlient/config/common"
+	runtimeClient "sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/go-logr/logr"
 	"github.com/konpyutaika/nifikop/api/v1alpha1"
@@ -156,6 +157,7 @@ done`,
 			Affinity: &corev1.Affinity{
 				PodAntiAffinity: generatePodAntiAffinity(r.NifiCluster.Name, r.NifiCluster.Spec.OneNifiNodePerNode),
 			},
+			TopologySpreadConstraints:     r.NifiCluster.Spec.TopologySpreadConstraints,
 			Containers:                    r.injectAdditionalEnvVars(r.generateContainers(nodeConfig, id, podVolumeMounts, zkAddress)),
 			Volumes:                       podVolumes,
 			RestartPolicy:                 corev1.RestartPolicyNever,
