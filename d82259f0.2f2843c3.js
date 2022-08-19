917ada5c0bbd51a5ec772e357bd95e1d602deb88
erdrix <10503351+erdrix@users.noreply.github.com>
2022-08-19 20:29:17 +0000

    [skip ci]

---
+++ d82259f0.2f2843c3.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[442],{508:function(n){n.exports=JSON.parse('{"allTagsPath":"/nifikop/blog/tags","slug":"gke","name":"gke","count":2,"permalink":"/nifikop/blog/tags/gke"}')}}]);
