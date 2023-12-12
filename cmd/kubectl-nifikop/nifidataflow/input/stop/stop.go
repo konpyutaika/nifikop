0a14e4b58c3df7637363de32d99d409688b8ca84
Juldrixx <31806759+juldrixx@users.noreply.github.com>
2023-12-12 13:58:22 +0100

    Replace Update by Patch on K8S resource (#333)
    * Replace Update by Patch on K8S resource
    
    * Update CHANGELOG
    
    * Update CHANGELOG.md
    
    Co-authored-by: Michael H <86672176+mh013370@users.noreply.github.com>
    
    * Add OptimisticLock on MergeFrom
    
    ---------
    
    Co-authored-by: Michael H <86672176+mh013370@users.noreply.github.com>

--- cmd/kubectl-nifikop/nifidataflow/input/stop/stop.go
+++ cmd/kubectl-nifikop/nifidataflow/input/stop/stop.go
@@ -112,7 +112,7 @@ func (o *options) run(cmd *cobra.Command) error {
 	}
 
 	item.SetLabels(labels)
-	err = o.Client.Patch(context.TODO(), item, client.MergeFrom(itemOriginal))
+	err = o.Client.Patch(context.TODO(), item, client.MergeFromWithOptions(itemOriginal, client.MergeFromWithOptimisticLock{}))
 
 	if err != nil {
 		cmd.Println(fmt.Sprintf("Couldn't patch %s/%s: %v", item.GetNamespace(), item.GetName(), err))
