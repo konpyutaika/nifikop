fccafa8abfaa29c3739819441d68ebfababe62bf
Julien Guitton <julien.guitton@orange.com>
2024-01-08 17:13:57 +0100

    Update docs

--- api/v1/common_types.go
+++ api/v1/common_types.go
@@ -275,9 +275,9 @@ const (
 )
 
 const (
-	// ZookeeperClusterManager indicates that the cluster election and states will be managed with ZooKeeper.
+	// ZookeeperClusterManager indicates that the cluster leader election and state management will be managed with ZooKeeper.
 	ZookeeperClusterManager ClusterManagerType = "zookeeper"
-	// ZookeeperClusterManager indicates that the cluster election and states will be managed with Kubernetes resources,
+	// ZookeeperClusterManager indicates that the cluster leader election and state management will be managed with Kubernetes resources,
 	// respectively with Leases and ConfigMaps.
 	KubernetesClusterManager ClusterManagerType = "kubernetes"
 )
