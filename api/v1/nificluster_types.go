ff49b5a450993bede503011a0fb66d4d114bb4e8
Julien Guitton <julien.guitton@orange.com>
2024-01-11 16:57:31 +0100

    Update default value

--- api/v1/nificluster_types.go
+++ api/v1/nificluster_types.go
@@ -797,7 +797,7 @@ func (nConfig *NodeConfig) GetProvenanceStorage() string {
 	if nConfig.ProvenanceStorage != "" {
 		return nConfig.ProvenanceStorage
 	}
-	return "8 GB"
+	return "10 GB"
 }
 
 // GetNifiJvmMemory returns the default "2g" NifiJvmMemory if not specified otherwise.
