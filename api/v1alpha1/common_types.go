239a76b88b689f5cf65258dc3d7864eb40c6e95b
Juldrixx <31806759+juldrixx@users.noreply.github.com>
2024-03-13 19:24:15 +0100

    Fix typo (#404)
    * Fix typo
    
    * Update CHANGELOG

--- api/v1alpha1/common_types.go
+++ api/v1alpha1/common_types.go
@@ -29,7 +29,7 @@ type ConnectionState string
 // DataflowUpdateRequestType defines the type of versioned flow update request.
 type DataflowUpdateRequestType string
 
-// ComponentUpdateStrategy defines the type of strategy to update a component
+// ComponentUpdateStrategy defines the type of strategy to update a component.
 // +kubebuilder:validation:Enum={"drop","drain"}
 type ComponentUpdateStrategy string
 
@@ -51,15 +51,15 @@ type ConfigurationState string
 // InitClusterNode holds info about if the node was part of the init cluster setup.
 type InitClusterNode bool
 
-// PKIBackend represents an interface implementing the PKIManager
+// PKIBackend represents an interface implementing the PKIManager.
 // +kubebuilder:validation:Enum={"cert-manager","vault"}
 type PKIBackend string
 
-// ClientConfigType represents an interface implementing the ClientConfigManager
+// ClientConfigType represents an interface implementing the ClientConfigManager.
 // +kubebuilder:validation:Enum={"tls","basic"}
 type ClientConfigType string
 
-// ClusterType represents an interface implementing the  ClientConfigManager
+// ClusterType represents an interface implementing the ClientConfigManager.
 // +kubebuilder:validation:Enum={"external","internal"}
 type ClusterType string
 
